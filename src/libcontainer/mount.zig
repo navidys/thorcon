5fa41e3b205d908faffa493d7396f43486f93599
Navid Yaghoobi <navidys@fedoraproject.org>
2025-12-26 21:56:39 +1100

    Initial commit
    Signed-off-by: Navid Yaghoobi <navidys@fedoraproject.org>

--- src/libcontainer/mount.zig
+++ src/libcontainer/mount.zig
@@ -6,6 +6,7 @@ const posix = std.posix;
 const linux = std.os.linux;
 
 pub fn setContainerMountPoints(pid: i32, spec: ocispec.runtime.Spec) !void {
+    // TODO mount cgroup
     std.log.debug("pid {} setting container mount points", .{pid});
 
     if (spec.mounts) |mountPoints| {
@@ -18,6 +19,7 @@ pub fn setContainerMountPoints(pid: i32, spec: ocispec.runtime.Spec) !void {
             const mountPath = try posix.toPosixPath(mountPoint.destination);
             var mountSource = try std.fmt.allocPrintZ(std.heap.page_allocator, "none", .{});
             var mountType = try std.fmt.allocPrintZ(std.heap.page_allocator, "", .{});
+            var mountFlags: u32 = 0;
 
             if (mountPoint.source) |src| {
                 mountSource = try std.fmt.allocPrintZ(std.heap.page_allocator, "{s}", .{src});
@@ -27,13 +29,76 @@ pub fn setContainerMountPoints(pid: i32, spec: ocispec.runtime.Spec) !void {
                 mountType = try std.fmt.allocPrintZ(std.heap.page_allocator, "{s}", .{mtype});
             }
 
+            if (mountPoint.options) |options| {
+                for (options) |mopt| {
+                    if (std.mem.eql(u8, mopt, "nosuid")) {
+                        if (mountFlags == 0) {
+                            mountFlags = linux.MS.NOSUID;
+                        } else {
+                            mountFlags = mountFlags | linux.MS.NOSUID;
+                        }
+
+                        continue;
+                    }
+
+                    if (std.mem.eql(u8, mopt, "noexec")) {
+                        if (mountFlags == 0) {
+                            mountFlags = linux.MS.NOEXEC;
+                        } else {
+                            mountFlags = mountFlags | linux.MS.NOEXEC;
+                        }
+
+                        continue;
+                    }
+
+                    if (std.mem.eql(u8, mopt, "nodev")) {
+                        if (mountFlags == 0) {
+                            mountFlags = linux.MS.NODEV;
+                        } else {
+                            mountFlags = mountFlags | linux.MS.NODEV;
+                        }
+
+                        continue;
+                    }
+
+                    if (std.mem.eql(u8, mopt, "relatime")) {
+                        if (mountFlags == 0) {
+                            mountFlags = linux.MS.RELATIME;
+                        } else {
+                            mountFlags = mountFlags | linux.MS.RELATIME;
+                        }
+
+                        continue;
+                    }
+
+                    if (std.mem.eql(u8, mopt, "ro")) {
+                        if (mountFlags == 0) {
+                            mountFlags = linux.MS.RDONLY;
+                        } else {
+                            mountFlags = mountFlags | linux.MS.RDONLY;
+                        }
+
+                        continue;
+                    }
+
+                    if (std.mem.eql(u8, mopt, "noatime")) {
+                        if (mountFlags == 0) {
+                            mountFlags = linux.MS.NOATIME;
+                        } else {
+                            mountFlags = mountFlags | linux.MS.NOATIME;
+                        }
+
+                        continue;
+                    }
+                }
+            }
+
             // TODO create directotories
             // TODO check if type is bind then create parent directory
 
-            std.log.debug("pid {} mkdir {s}", .{ pid, mountPoint.destination });
             try utils.createDirAll(mountPoint.destination);
 
-            switch (linux.E.init(linux.mount(mountSource, &mountPath, mountType, 0, 0))) {
+            switch (linux.E.init(linux.mount(mountSource, &mountPath, mountType, mountFlags, 0))) {
                 .SUCCESS => {},
                 else => |err| {
                     std.log.debug("pid {} container mount error: {any}", .{ pid, err });
